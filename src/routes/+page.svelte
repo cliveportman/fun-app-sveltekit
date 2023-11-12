0070555aeba97f35d736c43626c24886f7a23ee5
cliveportman <clive@cliveportman.co.uk>
2023-11-12 17:51:50 +0000

    fetch data from api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<ul>
+  <li>{data.activity.name}</li>
+  <li>{data.activity.distance}</li>
+  <li>{data.activity.duration}</li>
+</ul>
