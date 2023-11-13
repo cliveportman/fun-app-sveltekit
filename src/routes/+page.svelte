4aacae5807f1c8da7dc67cb5ffa9666f194885f7
cliveportman <clive@cliveportman.co.uk>
2023-11-13 17:46:35 +0000

    install tailwind and dayjs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
   import type { PageData } from "./$types";
-
   export let data: PageData;
+
+  import dayjs from "dayjs";
+  import duration from "dayjs/plugin/duration";
+  dayjs.extend(duration);
 </script>
 
-<ul>
+<ul class="text-yellow-200 text-5xl font-bold leading-tight">
   <li>{data.activity.name}</li>
-  <li>{data.activity.distance}</li>
-  <li>{data.activity.duration}</li>
+  <li>
+    {(data.activity.distance / 1000).toLocaleString("en-GB", {
+      minimumFractionDigits: 1,
+      maximumFractionDigits: 1,
+    })} km
+  </li>
+  <li>
+    {dayjs
+      .duration(data.activity.duration, "seconds")
+      .format("H [hrs] m [min]")}
+  </li>
 </ul>
