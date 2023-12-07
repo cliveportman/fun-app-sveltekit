c01de45c8e11c1d998ac533b9b8858b7c05a60ee
Clive Portman <clive@cliveportman.co.uk>
2023-12-07 10:07:35 +0000

    Adding logic functions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,22 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+// or
+function or(a: boolean, b: boolean): boolean {
+  return a || b;
+}
+
+// not
+function not(a: boolean, b: boolean): boolean {
+  return !a && !b;
+}
+
+// exclusive or
+function xor(a: boolean, b: boolean): boolean {
+  return a !== b;
+}
+
+// and
+function and(a: boolean, b: boolean): boolean {
+  return a && b;
+}
+
